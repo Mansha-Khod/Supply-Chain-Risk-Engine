0a009442a1d7c97a5436e90a79f3707566d96a34
Mansha-Khod <manshakhod@gmail.com>
2026-07-23 10:32:59 +0530

    limited the cache size to 1

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,3 +1,16 @@
 from functools import lru_cache
-from app.config import XGB_MODEL,RF_MODEL,SCALER_PATH,SHAP_EXPLAINER_PATH
+from app.config import XGB_MODEL_PATH,RF_MODEL_PATH,SCALER_PATH,SHAP_EXPLAINER_PATH
+import joblib
 
+class Models:
+    def __init__(self):
+        self.xgb_model=joblib.load(XGB_MODEL_PATH)
+        self.rf_model=joblib.load(RF_MODEL_PATH)
+        self.scaler=joblib.load(SCALER_PATH)
+        self.explainer=joblib.load(SHAP_EXPLAINER_PATH)
+        self.xgb_feature_names=self.xgb_model.get_booster().feature_names
+
+
+@lru_cache(maxsize=1)
+def get_models()->Models:
+    return Models()       
